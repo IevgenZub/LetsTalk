437d2b239d8bcb92e5cd6b84cb36b5ae56a3cd2e
Ievgen Zubkov <euginzub@mail.ru>
2016-01-14 16:11:14 +0200

    getLoginStatus used

--- src/LetsTalk/wwwroot/js/controllers/chatController.js
+++ src/LetsTalk/wwwroot/js/controllers/chatController.js
@@ -56,12 +56,28 @@
             }, 1000);
 
             setTimeout(function () {
-                facebookService.getUserInfo().then(function (response) {
-                    $scope.userInfo = response;
-                    
-                    renderCurrentUserLocation();
+                FB.getLoginStatus(function (response) {
+                    if (response.status === 'connected') {
+                        // the user is logged in and has authenticated your
+                        // app, and response.authResponse supplies
+                        // the user's ID, a valid access token, a signed
+                        // request, and the time the access token 
+                        // and signed request each expire
+                        var uid = response.authResponse.userID;
+                        var accessToken = response.authResponse.accessToken;
+
+                        facebookService.getUserInfo().then(function (response) {
+                            $scope.userInfo = response;
+                            renderCurrentUserLocation();
+                        });
+                    } else if (response.status === 'not_authorized') {
+                        // the user is logged in to Facebook, 
+                        // but has not authenticated your app
+                    } else {
+                        // the user isn't logged in to Facebook.
+                    }
                 });
-            }, 1000);
+            }, 2000);
         }
 
         function renderCurrentUserLocation() {
